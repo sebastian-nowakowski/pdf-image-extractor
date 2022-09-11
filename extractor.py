4819a495ca875ec9504525a2f8b098bc859cd92e
g4rr3t <s.nowakowski82@gmail.com>
2022-09-11 20:35:59 +0200

    comments + requirements

--- extractor.py
+++ extractor.py
@@ -1,9 +1,12 @@
 import sys
-import fitz # PyMuPDF
+import fitz
 import io
 from PIL import Image
 
 def extract(path: str, output_path: str):
+    """[summary]
+    Extracts images from pdf file at [path] to [output_path]/
+    """
     file = fitz.open(path)
     if file:
         for page_index in range(len(file)):
@@ -12,7 +15,7 @@ def extract(path: str, output_path: str):
             image_list = page.get_images()
             # printing number of images found in this page
             if image_list:
-                print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
+                print(f'[+] Found a total of {len(image_list)} images in page {page_index}')
             else:
                 print("[!] No images found on page", page_index)
             for image_index, img in enumerate(page.get_images(), start=1):
@@ -20,4 +23,5 @@ def extract(path: str, output_path: str):
                 pix0 = fitz.Pixmap(fitz.csRGB, pix)
                 pix0.save(f'{output_path}/img{img[0]}.jpg')
 
+# gets arguments from command line
 extract(sys.argv[1], sys.argv[2])
